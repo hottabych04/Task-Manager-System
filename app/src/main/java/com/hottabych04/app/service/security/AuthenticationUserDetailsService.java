ec8308708e5159dd0375deccc4963168fd6a72f0
hottabych04 <hottabych04@gmail.com>
2024-12-30 23:13:27 +0300

    Create UserDetailsService

---
+++ app/src/main/java/com/hottabych04/app/service/security/AuthenticationUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.hottabych04.app.service.security;
+
+import com.hottabych04.app.database.repository.UserRepository;
+import com.hottabych04.app.exception.user.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
