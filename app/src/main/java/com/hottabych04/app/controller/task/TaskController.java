f2926df036e1e8ba377d0a03b6f0ae9526110d04
hottabych04 <hottabych04@gmail.com>
2025-01-10 12:50:04 +0300

    Delete performers controller

--- app/src/main/java/com/hottabych04/app/controller/task/TaskController.java
+++ app/src/main/java/com/hottabych04/app/controller/task/TaskController.java
@@ -2,6 +2,7 @@ package com.hottabych04.app.controller.task;
 
 import com.hottabych04.app.controller.task.payload.TaskCreateDto;
 import com.hottabych04.app.controller.task.payload.TaskGetDto;
+import com.hottabych04.app.service.task.PerformerService;
 import com.hottabych04.app.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,12 +11,15 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
 
     private final TaskService taskService;
+    private final PerformerService performerService;
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
@@ -41,6 +45,24 @@ public class TaskController {
             @RequestParam(value = "size", defaultValue = "5") Integer size
     ){
         return taskService.handleGetRequest(author, performer, page, size);
+    }
+
+    @PatchMapping("/{id}/performers")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void addPerformer(
+            @PathVariable Long id,
+            @RequestBody List<String> performers
+    ){
+        performerService.addPerformers(id, performers);
+    }
+
+    @DeleteMapping("/{id}/performers")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deletePerformer(
+            @PathVariable Long id,
+            @RequestParam("email") String performer
+    ){
+        performerService.deletePerformer(id, performer);
     }
 
     @DeleteMapping("/{id}")
